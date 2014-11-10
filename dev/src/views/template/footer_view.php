77c989beb1261f2e6b9b1fcc09f6ca1f92d36c54
iliassk <kenfaoui.iliass@gmail.com>
2014-11-10 19:35:37 +0100

    [end Vheader][end Vmenu] Ajout des table sorters

--- dev/src/views/template/footer_view.php
+++ dev/src/views/template/footer_view.php
@@ -5,5 +5,7 @@
     <script src="<?php echo base_url(); ?>assets/js/jquery-1.10.2.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/das.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/tablesorter/jquery.tablesorter.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/tablesorter/tables.js"></script>
 </body>
 </html>
