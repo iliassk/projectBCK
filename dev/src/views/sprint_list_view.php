018f4d784688c4ead0958b5e08a937479509e372
bchalaux <bchalaux33@gmail.com>
2014-11-20 18:11:27 +0100

    ajout de la page de transition entre liste de sprints et les pages liés à un sprint

--- dev/src/views/sprint_list_view.php
+++ dev/src/views/sprint_list_view.php
@@ -1,35 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+
     <title> ScumIT - Liste des Sprints </title>
-    <style>
-        .square {
-            float:left;
-            position: relative;
-            width: 25%;
-            padding-bottom : 25%; /* = width for a 1:1 aspect ratio */
-            margin:2%;
-            background-color: powderblue;
-            overflow:hidden;
-        }
-        .content {
-            position:absolute;
-            height:90%; /* = 100% - 2*5% padding */
-            width:90%; /* = 100% - 2*5% padding */
-            padding: 5%;
-        }
-        .table{
-            display:table;
-            width:100%;
-            height:100%;
-        }
-        .table-cell{
-            display:table-cell;
-            vertical-align:middle;
-            text-align: center;
-        }
-    </style>
-</head>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/square.css" />
+
 
 <?php
 $this->load->view("template/header_view");
@@ -40,24 +13,24 @@ $this->load->view("template/nav_view");
     <div class="row">
         <div style="width:800px; margin:0 auto;" class="col-lg-12">
             <h1 class = "page-header">Liste des sprints</small></h1>
-            <div id="wrapper">
-
-                        <?php
-                        foreach($data as $row)
-                            echo '<div class=square>
-                                <div class=content>
-                                <div class=table>
-                                <div class=table-cell>'.
-                                '<a href='.base_url().'tasks/init/'.$idPro.'/'.$row->idSprint.'>Sprint '. $row->idSprint.'</a>'.
-                                '</div></div></div></div>'
-                        ?>
-
-            </div>
         </div>
     </div>
+
+        <div style="width:800px; margin:0 auto;" class="col-lg-24">
+
+            <?php
+
+            foreach($data as $row)
+                echo '<a href='.base_url().'sprint/index/'.$idPro.'/'.$row->idSprint.'>'.
+                    '<div class=square> <div class=content>
+                    <div class=table> <div class=table-cell>'.
+                    'Sprint '. $row->idSprint.
+                    '</div></div></div></div> </a>';
+            ?>
+
+
+        </div>
 </div>
 
 <?php $this->load->view("template/footer_view");?>
 
-
-</html>
---
+++ dev/src/views/sprint_view.php
@@ -0,0 +1,54 @@
+
+    <title> ScumIT - Sprint </title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/square.css" />
+
+
+<?php
+$this->load->view("template/header_view");
+$this->load->view("template/nav_view");
+?>
+
+<div id="page-wrapper">
+    <div class="row">
+        <div style="width:800px; margin:0 auto;" class="col-lg-12">
+            <h1 class = "page-header">Sprint <?php echo $idSprint;?></small></h1>
+        </div>
+    </div>
+
+    <div style="width:800px; margin:0 auto;" class="col-lg-24">
+
+        <?php
+
+            echo '<a href='.base_url().'tasks/index/'.$idPro.'/'.$idSprint.'>'.
+                '<div class=square> <div class=content>
+                 <div class=table> <div class=table-cell> Tâches '.
+                '</div></div></div></div> </a>';
+
+            echo '<a href='.base_url().'gantt/index/'.$idPro.'/'.$idSprint.'>'.
+                '<div class=square> <div class=content>
+                 <div class=table> <div class=table-cell> Gantt '.
+                '</div></div></div></div> </a>';
+
+            echo '<a href='.base_url().'kanban/index/'.$idPro.'/'.$idSprint.'>'.
+                '<div class=square> <div class=content>
+                 <div class=table> <div class=table-cell> Kanban '.
+                '</div></div></div></div> </a>';
+
+            echo '<a href='.base_url().'test/index/'.$idPro.'/'.$idSprint.'>'.
+                '<div class=square> <div class=content>
+                 <div class=table> <div class=table-cell> Tests '.
+                '</div></div></div></div> </a>';
+
+            echo '<a href='.base_url().'burndownchart/index/'.$idPro.'/'.$idSprint.'>'.
+                '<div class=square> <div class=content>
+                 <div class=table> <div class=table-cell> Burn Down Chart '.
+                '</div></div></div></div> </a>';
+
+        ?>
+
+    </div>
+</div>
+
+<?php $this->load->view("template/footer_view");?>
+
