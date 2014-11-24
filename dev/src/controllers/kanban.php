dece3350d0dce8cbd7fe28a6b426a1b5c11e28fb
iliassk <kenfaoui.iliass@gmail.com>
2014-11-24 21:40:29 +0100

    [end Ckanban][end Mkanban] Modification du model et rajout de la génération beta des boards

--- dev/src/controllers/kanban.php
+++ dev/src/controllers/kanban.php
@@ -30,5 +30,41 @@ class Kanban extends CI_Controller {
         $this->load->view('kanban/index', $data);
     }
 
+	private function generateBoardCards($backlogTasks, $id, $typeOfTasks){
+		$html = "";
+		$html .= '<ul id="card'.$id.'" class="card card-container panel panel-primary" data-id="'.$id.'" >'.
+	      '<div class="card-name panel-heading">'.
+	      	'<label class="card-name-label">'.
+	      		$typeOfTasks.
+	      	'</label>'.
+	      	'<div class="menu pull-right">
+                    <div class="btn-group">                    
+                    </div>
+                </div>'.
+	      '</div>'.
+	      '<div class="card-name-input panel-heading" style="display:none;">'.
+	      	'<input type="text" class="card-name-update" id="cardNameUpdate" placeholder="Name" value="'.$typeOfTasks.'">'.
+	      '</div>'.
+	      '<div class="panel-body">'.
+		      "<ul class='tasks'>";
+		      //now loop through to retrieve all the tasks for the cards..
+		      foreach($backlogTasks->result() as $taskRow){
+		      	$html .= "<li id='task".$taskRow->idTask."' class='task' data-id='".$taskRow->idTask."' data-order='".$taskRow->order."' href='#detailedTask' data-toggle='modal' data-target='#taskModal'>";
+		      	$html .= '<span class="title-content">';
+		      	$html .= htmlentities($taskRow->nameTask).'';
+		      	$html .= '</span>';
+		      	$html .= "</li>";
+		      }
+
+	      $html .= "</ul>".
+	      '</div>'.
+		  '<div class="add-task-input-container panel-footer" style="display:none;">'.
+		  	'<textarea class="add-task-input" id="addTaskInput" placeholder="Task"></textarea>'.
+	      '</div>'.
+	      '</ul>';	
+
+		return $html;
+	}
+
 
 }
